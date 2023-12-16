d1d39d16ea6c7d375428c9ddc3f14a833cc536bf
Richard <richardzehungho@gmail.com>
2023-12-15 16:00:41 -0800

    Added logic checks for valid inputs

--- app.js
+++ app.js
@@ -18,12 +18,44 @@ const currentDateInMonth = (month, year) => {
   return new Date(year, month, 0).getDay();
 };
 
-const inputValidation = (day, year, month) => {
-  if (day > daysInMonth(month, year) || day === "") {
-    console.log("invalid");
-  } else {
-    console.log("valid");
+const inputValidation = (day, year, month, currentYear) => {
+  if (
+    day <= 31 ||
+    (day >= 1 && month <= 12) ||
+    (month >= 1 && year <= currentYear)
+  ) {
+    removeErrorColors();
   }
+  if (day === "" && month === "" && year === "") {
+    showErrorInput();
+    errorEmptyField();
+  }
+
+  if (day > 31 || day < 1 || day > daysInMonth(month, year)) {
+    showErrorInput();
+    errorDayField();
+  } else {
+    clearErrorMsgDay("day");
+    // removeErrorColors();
+  }
+  if (month > 12 || month < 1) {
+    showErrorInput();
+    errorMonthField();
+  } else {
+    clearErrorMsgDay("month");
+  }
+  if (year > currentYear) {
+    showErrorInput();
+    errorYearField();
+  } else {
+    clearErrorMsgDay("year");
+  }
+
+  // if (day > daysInMonth(month, year) || day === "") {
+  //   console.log("invalid");
+  // } else {
+  //   console.log("valid");
+  // }
 };
 
 const formSubmit = (() => {
@@ -32,14 +64,9 @@ const formSubmit = (() => {
 
   form.addEventListener("submit", (e) => {
     e.preventDefault();
-    // console.log(daysInMonth(5, 2023));
-    // console.log(variables.dayInput.value);
-    // console.log(variables.monthInput.value);
-    // console.log(variables.yearInput.value);
-
-    let userDay = parseInt(variables.dayInput.value);
-    let userMonth = parseInt(variables.monthInput.value);
-    let userYear = parseInt(variables.yearInput.value);
+    const userDay = parseInt(variables.dayInput.value);
+    const userMonth = parseInt(variables.monthInput.value);
+    const userYear = parseInt(variables.yearInput.value);
 
     let currentDay = date.getDate();
     let currentMonth = date.getMonth() + 1;
@@ -59,56 +86,92 @@ const formSubmit = (() => {
       currentMonth = currentMonth + 12;
     }
 
-    console.log(currentDay);
-    console.log(userDay);
-    console.log(currentMonth);
-
     resultDay = currentDay - userDay;
     resultMonth = currentMonth - userMonth;
     resultYear = currentYear - userYear;
 
     console.log(resultDay + "day", resultMonth + "month", resultYear + "year");
 
-    //to find month
-    //if month <= current month and days >= current date
-    //ignore the current month and go back one month
-    //find the difference between days using current month days
+    variables.year.textContent = resultYear;
+    variables.month.textContent = resultMonth;
+    variables.day.textContent = resultDay;
 
-    // console.log(date.getDate());
-
-    // if (date.getMonth() + 1 - month <= month) {
-    //   console.log(month);
-    // }
-
-    // console.log(date.getMonth());
-
-    // if (date.getMonth() + 1 <= month)
-
-    // if (date.getMonth() + 1 - month <= month && day >= date.getDate()) {
-    //   // console.log("hi");
-    //   console.log(date.getFullYear() - year - 1 + "year");
-    //   console.log(month - date.getMonth() + "month");
-    //   console.log(daysInMonth(month, year) - day + date.getDate() + "day");
-    // } else {
-    //   console.log(date.getFullYear() - year + "year");
-    //   console.log(date.getMonth() + 1 - month + "month");
-    //   console.log(date.getDate() - day);
-    // }
-
-    //to find days remaining
-    //
-    // console.log(daysInMonth(12, 2023));
-    // console.log(daysInMonth(12, 2023) - date.getDate() - 4);
-    // console.log(daysInMonth(12, 2023) - parseInt(variables.dayInput.value));
+    // showErrorInput();
 
     inputValidation(
       variables.dayInput.value,
       variables.yearInput.value,
-      variables.monthInput.value
+      variables.monthInput.value,
+      currentYear
     );
-
-    variables.year.textContent = variables.yearInput.value;
-    variables.month.textContent = variables.monthInput.value;
-    variables.day.textContent = variables.dayInput.value;
   });
 })();
+
+const displayError = () => {
+  variables.year.textContent = "--";
+  variables.month.textContent = "--";
+  variables.day.textContent = "--";
+};
+
+const errorEmptyField = () => {
+  const errorDay = document.querySelector(".error.day");
+  const errorMonth = document.querySelector(".error.month");
+  const errorYear = document.querySelector(".error.year");
+
+  errorDay.textContent = "This field is required";
+  errorMonth.textContent = "This field is required";
+  errorYear.textContent = "This field is required";
+
+  displayError();
+};
+
+const clearErrorMsgDay = (input) => {
+  const errorDay = document.querySelector(`.error.${input}`);
+
+  errorDay.textContent = "";
+};
+
+const removeErrorColors = () => {
+  const label = document.querySelectorAll("label");
+  const input = document.querySelectorAll("input");
+
+  label.forEach((item) => {
+    item.style.color = "hsl(0, 1%, 44%)";
+  });
+
+  input.forEach((item) => {
+    item.style.borderColor = "hsl(0, 0%, 86%)";
+  });
+};
+
+const showErrorInput = () => {
+  const label = document.querySelectorAll("label");
+  const input = document.querySelectorAll("input");
+
+  label.forEach((item) => {
+    item.style.color = "hsl(0, 100%, 67%)";
+  });
+
+  input.forEach((item) => {
+    item.style.borderColor = "hsl(0, 100%, 67%)";
+  });
+};
+
+const errorDayField = () => {
+  const errorDay = document.querySelector(".error.day");
+  errorDay.textContent = "Must be a valid day";
+
+  displayError();
+};
+
+const errorMonthField = () => {
+  const errorMonth = document.querySelector(".error.month");
+  errorMonth.textContent = "Must be a valid month";
+  displayError();
+};
+
+const errorYearField = () => {
+  const errorYear = document.querySelector(".error.year");
+  errorYear.textContent = "Must be current or past year";
+  displayError();
+};
